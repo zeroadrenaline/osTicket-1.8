ae9090d64aa1614ece629b976d7e9b9fbd33eee2
Robin Toy <robin@strobe-it.co.uk>
2015-03-14 17:07:45 +0000

    settings.php - Update
    Updated to version v1.9.6

--- scp/tickets.php
+++ scp/tickets.php
@@ -59,15 +59,16 @@ if($_POST && !$errors):
         $errors=array();
         $lock=$ticket->getLock(); //Ticket lock if any
 		
-		// Strobe Technologies Ltd | 20/10/2014 | START - Collect Total Spent from results
+		// Strobe Technologies Ltd | 14/03/2015 | START - Collect Total Spent from results
+		// osTicket Version = v1.9.6
 		if($_POST['time_spent']) {
 			$ticket->timeSpent($_POST['time_spent']);
 		}
-		// Strobe Technologies Ltd | 20/10/2014 | END - Collect Total Spent from results
+		// Strobe Technologies Ltd | 14/03/2015 | END - Collect Total Spent from results
 		
         switch(strtolower($_POST['a'])):
-		
-		// Strobe Technologies Ltd | 22/10/2014 | START - Add time case / switch
+		// Strobe Technologies Ltd | 14/03/2015 | START - Add time case / switch
+		// osTicket Version = v1.9.6
 		case 'time':
 			if(!$_POST['time_spent'])
 				$errors['time_spent']=__('Time required');
@@ -103,7 +104,7 @@ if($_POST && !$errors):
                 $errors['err']=__('Unable to post the time. Correct the errors below and try again!');
             }
 			break;
-			// Strobe Technologies Ltd | 22/10/2014 | END - Add time case / switch
+			// Strobe Technologies Ltd | 14/03/2015 | END - Add time case / switch
         case 'reply':
             if(!$thisstaff->canPostReply())
                 $errors['err'] = __('Action denied. Contact admin for access');
